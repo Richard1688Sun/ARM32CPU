bf1365c40a24f1fd39d553db479e2a917ab88389
Richard Sun <richardsun.gz@gmail.com>
2024-04-21 22:35:50 -0700

    changed values for integrated_cpu + moved testbenches

--- pipeline/modules_rtl/integrated_cpu.sv
+++ pipeline/modules_rtl/integrated_cpu.sv
@@ -14,15 +14,14 @@ module integrated_cpu(input logic CLOCK_50, input logic [3:0] KEY, input logic [
 
     // cpu outputs
     wire waiting;
-    wire ram_w_en1;
-    wire ram_w_en2;
+    wire mem_w_en;
     wire [10:0] ram_addr2;
     wire [31:0] ram_in2;
-    wire [31:0] status_out;
-    wire [31:0] datapath_out;
+    wire [31:0] status;
+    wire [31:0] dp_out;
 
     // pc outputs
-    wire [10:0] pc_out;
+    wire [10:0] pc;
 
     // duel_ram outputs
     wire [31:0] ram_data1;
@@ -31,35 +30,33 @@ module integrated_cpu(input logic CLOCK_50, input logic [3:0] KEY, input logic [
     //TODO: remove later
     wire [31:0] reg_output;
     assign LEDR = reg_output[9:0];
-    assign HEX0 = (sel_instr == 1'b1) ? ram_data1[6:0] : status_out[6:0];
-    assign HEX1 = (sel_instr == 1'b1) ? ram_data1[13:7] : status_out[13:7];
-    assign HEX2 = (sel_instr == 1'b1) ? ram_data1[20:14] : status_out[20:14];
-    assign HEX3 = (sel_instr == 1'b1) ? ram_data1[27:21] : status_out[27:21];
-    assign HEX4 = (sel_instr == 1'b1) ? ram_data1[31:28] : status_out[31:28];
+    assign HEX0 = (sel_instr == 1'b1) ? ram_data1[6:0] : status[6:0];
+    assign HEX1 = (sel_instr == 1'b1) ? ram_data1[13:7] : status[13:7];
+    assign HEX2 = (sel_instr == 1'b1) ? ram_data1[20:14] : status[20:14];
+    assign HEX3 = (sel_instr == 1'b1) ? ram_data1[27:21] : status[27:21];
+    assign HEX4 = (sel_instr == 1'b1) ? ram_data1[31:28] : status[31:28];
 
     // cpu module
     cpu cpu(
         .clk(clk),
         .rst_n(rst_n),
         .instr(ram_data1),
-        .start_pc(11'b0),
         .ram_data2(ram_data2),
-        .waiting(waiting),
-        .ram_w_en1(ram_w_en1),
-        .ram_w_en2(ram_w_en2),
+        .start_pc(11'b0),
+        .mem_w_en(mem_w_en),
         .ram_addr2(ram_addr2),
         .ram_in2(ram_in2),
-        .status_out(status_out),
-        .datapath_out(datapath_out),
-        .pc_out(pc_out),
+        .status(status),
+        .dp_out(dp_out),
+        .pc(pc),
         .reg_output(reg_output), .reg_addr(SW[3:0]) //TODO: remove later
     );
 
     //instruction_memory module
     instr_mem instruction_memory(
         .clock(clk),
-        .wren(ram_w_en1),
-        .address(pc_out[6:0]),
+        .wren(1'b0),
+        .address(pc[6:0]),
         .data(32'b0),
         .q(ram_data1)
     );
@@ -67,7 +64,7 @@ module integrated_cpu(input logic CLOCK_50, input logic [3:0] KEY, input logic [
     // data_memory module
     data_mem data_memory(
         .clock(clk),
-        .wren(ram_w_en2),
+        .wren(mem_w_en),
         .address(ram_addr2[6:0]),
         .data(ram_in2),
         .q(ram_data2)
