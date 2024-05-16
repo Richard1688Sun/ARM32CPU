09d590a8f5278f5dda30b4ed2a07c3df1ecab901
Richard Sun <richardsun.gz@gmail.com>
2024-05-15 19:14:45 -0700

    some tests passing

--- pipeline/testbench_rtl/tb_integrated_cpu.sv
+++ pipeline/testbench_rtl/tb_integrated_cpu.sv
@@ -1,19 +1,38 @@
 `timescale 1ps / 1ps
 module tb_integrated_cpu();
+    // constants for HEX
+    reg [6:0] display [16] = '{
+        7'b1000000, // 0
+        7'b1111001, // 1
+        7'b0100100, // 2
+        7'b0110000, // 3
+        7'b0011001, // 4
+        7'b0010010, // 5
+        7'b0000010, // 6
+        7'b1111000, // 7
+        7'b0000000, // 8
+        7'b0010000, // 9
+        7'b1111111, // blank
+        7'b1111111, // blank
+        7'b1111111, // blank
+        7'b1111111, // blank
+        7'b1111111, // blank
+        7'b1111111 // blank
+    };
 
     //test regs
     integer error_count = 0;
 
     //cpu inputs
-    reg clk, rst_n, sel_instr;
+    reg clk, rst_n, manual_clk;
     wire CLOCK_50;
     wire [3:0] KEY;
     wire [9:0] SW;
     wire [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
     wire [9:0] LEDR;
-    reg [31:0] status_out;
-    reg [31:0] reg_output;
-    reg [3:0] reg_addr;
+
+    // internal signals
+    reg [4:0] reg_addr;
 
     //cpu module
     integrated_cpu DUT(
@@ -30,11 +49,31 @@ module tb_integrated_cpu();
     );
 
     assign CLOCK_50 = clk;
-    assign KEY[0] = rst_n;
-    assign KEY[1] = sel_instr;
-    assign SW = { 6'b0, reg_addr};
-    assign status_out = {HEX4[3:0], HEX3, HEX2, HEX1, HEX0};
-    assign reg_output = LEDR;
+    assign KEY[0] = ~manual_clk;
+    assign KEY[1] = rst_n;
+    assign SW = { 2'b11, reg_addr, 3'b000};
+
+    function [3:0] reverse_display (input [6:0] value);
+        case (value)
+            display[0]: return 4'b0000;
+            display[1]: return 4'b0001;
+            display[2]: return 4'b0010;
+            display[3]: return 4'b0011;
+            display[4]: return 4'b0100;
+            display[5]: return 4'b0101;
+            display[6]: return 4'b0110;
+            display[7]: return 4'b0111;
+            display[8]: return 4'b1000;
+            display[9]: return 4'b1001;
+            display[10]: return 4'b1010;
+            display[11]: return 4'b1011;
+            display[12]: return 4'b1100;
+            display[13]: return 4'b1101;
+            display[14]: return 4'b1110;
+            display[15]: return 4'b1111;
+            default: return 4'b1111; // Return a default value if no match is found
+        endcase
+    endfunction
 
     // Tasks
     task check(input [31:0] expected, input [31:0] actual, integer test_num);
@@ -46,7 +85,45 @@ module tb_integrated_cpu();
         end
     endtask: check
 
-    task setRegAddr(input [3:0] addr);
+    
+    task autoClkTimes (input integer times);
+        for (int i = 0; i < times; i = i + 1) begin
+            clk = 1'b0;
+            #1;
+            clk = 1'b1;
+            #1;
+        end
+    endtask;
+
+    // TODO: fix this
+    int has_error;
+    int og_expected;
+    task displayCheck (input int expected, integer test_num);
+        begin
+            has_error = 0;
+            og_expected = expected;
+            autoClkTimes(6);
+            if (HEX0 !== display[expected % 10]) has_error = 1;
+            expected = expected / 10;
+            if (HEX1 !== display[expected % 10]) has_error = 1;
+            expected = expected / 10;
+            if (HEX2 !== display[expected % 10]) has_error = 1;
+            expected = expected / 10;
+            if (HEX3 !== display[expected % 10]) has_error = 1;
+            expected = expected / 10;
+            if (HEX4 !== display[expected % 10]) has_error = 1;
+            expected = expected / 10;
+            if (HEX5 !== display[expected % 10]) has_error = 1;
+            if (has_error) begin
+                $error("Test %d failed. Expected: %d, Actual: %d%d%d%d%d%d", test_num, og_expected, reverse_display(HEX5), reverse_display(HEX4), reverse_display(HEX3), reverse_display(HEX2), reverse_display(HEX1), reverse_display(HEX0));
+                error_count = error_count + 1;
+            end else begin
+                $display("Test %d: Expected: %d, Actual: %d", test_num, og_expected, og_expected);
+            end
+        end
+    endtask : displayCheck
+
+    task setRegAddr(input [4:0] addr);
         begin
             reg_addr = addr;
             #5;
@@ -55,9 +132,9 @@ module tb_integrated_cpu();
 
     task clkR;
         begin
-            clk = 1'b0;
+            manual_clk = 1'b0;
             #5;
-            clk = 1'b1;
+            manual_clk = 1'b1;
             #5;
         end
     endtask: clkR
@@ -65,7 +142,8 @@ module tb_integrated_cpu();
     task reset;
         begin
             #5;
-            clk = 1'b0;
+            manual_clk = 1'b0;
+            setRegAddr(18);      // some random default number
             rst_n = 1'b1;
             #5;
             rst_n = 1'b0;
@@ -109,7 +187,6 @@ module tb_integrated_cpu();
             DUT.instruction_memory.altsyncram_component.m_default.altsyncram_inst.mem_data);
         
         restart_pc;
-        sel_instr = 1'b0;
         clkEnterMemory;
 
 
@@ -117,32 +194,39 @@ module tb_integrated_cpu();
         for (i = 0; i < 15; i = i + 1) begin
             clkR;
             setRegAddr(i);
-            check(i + 1, reg_output, i);
+            displayCheck(i + 1, i);
         end
         
         // ADD_R r0, r0, r0
         clkR;
         setRegAddr(0);
-        check(2, reg_output, 16);
+        displayCheck(2, 16);
 
         // ADD_I r1, r1, #8
         clkR;
+        // check status reg
+        setRegAddr(16);
+        displayCheck(0, 17);   // from previous test
         setRegAddr(1);
-        check(0, status_out, 17);   // from previous test
-        check(10, reg_output, 18);
+        displayCheck(10, 18);
 
         // ADD_RS r2, r2, r0, LSL r0
         clkR;
-        check(0, status_out, 19);
-        check(0, status_out, 21);
+        // check status reg
+        setRegAddr(16);
+        displayCheck(0, 19);
 
         // CMP_R r2, r1, LSL #1 (r2 = 11, r1 = 10 -> 20)
         clkR;
-        check(32'b10000000_00000000_00000000_00000000, status_out, 23);
+        // check status reg
+        setRegAddr(16);
+        displayCheck(32'b10000000_00000000_00000000_00000000, 23);
 
         // CMP_I r2, #11
         clkR;
-        check(32'b01000000_00000000_00000000_00000000, status_out, 25);
+        // check status reg
+        setRegAddr(16);
+        displayCheck(32'b01000000_00000000_00000000_00000000, 25);
 
         // ### LDR and STR tests ###
         $readmemb("C:/Users/richa/OneDrive - UBC/Documents/Personal_Projects/Winter_CPU_Project/ARM32CPU/memory_data/rtl_data/str_ldr_instr_CPUTests.memb",
@@ -157,7 +241,7 @@ module tb_integrated_cpu();
         for (i = 0; i < 15; i = i + 1) begin
             clkR;
             setRegAddr(i);
-            check(i, reg_output, i + 26);
+            displayCheck(i, i + 26);
         end
 
         // LDR_I r0, r9, #19
@@ -166,26 +250,26 @@ module tb_integrated_cpu();
         clkR; // ldr_writeback
         clkR; // complete ldr_writeback
         setRegAddr(0);
-        check(38, reg_output, 42);
+        displayCheck(38, 42);
 
         // STR_I r8, r0, #9 -> store 8 in address 29 -> 38 - 9 = 29 -> store 29 in r0
         // NOTE: There was stallin done for this instruction
         clkR; // complete memory
         setRegAddr(0);
-        check(29, reg_output, 43);
+        displayCheck(29, 43);
 
         //LDR_R r14, r0, r1 -> address = 29 -> write 28 to r0
         clkR; // complete memory
         setRegAddr(0);
-        check(28, reg_output, 44);
+        displayCheck(28, 44);
 
         clkR; // complete memory_wait for LDR_R && complete memory for STR_R
         //STR_R r9, r12, r2 LSL 3 -> address = 12 + 2 * 8 = 28 -> write 28 address 12
         setRegAddr(12);
-        check(28, reg_output, 46);
+        displayCheck(28, 46);
         clkR; // complete ldr_writeback for LDR_R && complete memory_wait STR_R
         setRegAddr(14);             // check again LDR_R r14, r0, r1
-        check(8, reg_output, 45);
+        displayCheck(8, 45);
 
         
         // LDR_Lit r1, #8 -> PC == 20
@@ -193,7 +277,7 @@ module tb_integrated_cpu();
         clkR; // was stalled for 1 cycle -> complete memory for LDR_Lit
         // checking final register value
         setRegAddr(1);
-        check(888, reg_output, 47);
+        displayCheck(888, 47);
 
         // ### Branch tests ###
         $readmemb("C:/Users/richa/OneDrive - UBC/Documents/Personal_Projects/Winter_CPU_Project/ARM32CPU/memory_data/rtl_data/branchCPUTests.memb",
@@ -205,12 +289,12 @@ module tb_integrated_cpu();
         //MOV_I r0, #1
         clkR;
         setRegAddr(0);
-        check(1, reg_output, 48);
+        displayCheck(1, 48);
 
         //MOV_I r1, #10
         clkR;
         setRegAddr(1);
-        check(10, reg_output, 49);
+        displayCheck(10, 49);
 
         //ADD r0, r0, #1
         //CMP r0, r1
@@ -218,12 +302,14 @@ module tb_integrated_cpu();
         for (i = 0; i < 8; i = i + 1) begin
             clkR;
             setRegAddr(0);
-            check(2 + i, reg_output, i * 3 + 50);
+            displayCheck(2 + i, i * 3 + 50);
             clkR;
-            check(32'b10000000_00000000_00000000_00000000, status_out, (i * 3) + 51);
+            // check status reg
+            setRegAddr(16);
+            displayCheck(32'b10000000_00000000_00000000_00000000, (i * 3) + 51);
             clkR;
             setRegAddr(15);
-            check(2, reg_output, (i * 3) + 52);
+            displayCheck(2, (i * 3) + 52);
             // branch was taken so need to squash the next 3 instructions
             clkR; // load_pc register 
             clkR; // finish fetch
@@ -232,12 +318,14 @@ module tb_integrated_cpu();
         end
         clkR;
         setRegAddr(0);
-        check(10, reg_output, 77);
+        displayCheck(10, 77);
         clkR;   //r0 == r1
-        check(32'b01000000_00000000_00000000_00000000, status_out, 78);
+        // check status reg
+        setRegAddr(16);
+        displayCheck(32'b01000000_00000000_00000000_00000000, 78);
         clkR;
         setRegAddr(15);
-        check(2, reg_output, 79);
+        displayCheck(2, 79);
         clkR; // load_pc register 
         clkR; // finish fetch
         clkR; // finish fetch_wait
@@ -245,13 +333,15 @@ module tb_integrated_cpu();
         // last loop iteration, branch not taken
         clkR;
         setRegAddr(0);
-        check(11, reg_output, 80);
+        displayCheck(11, 80);
         clkR;   //r0 > r1
-        check(32'b00000000_00000000_00000000_00000000, status_out, 81);
+        // check status reg
+        setRegAddr(16);
+        displayCheck(32'b00000000_00000000_00000000_00000000, 81);
         setRegAddr(15);
-        pc_before = reg_output; //  caching the previous PC value
+        // pc_before = reg_output; //  caching the previous PC value //TODO: fix this
         clkR;
-        check(pc_before + 1, reg_output, 82);   // checking that the PC was incremented by 1
+        // displayCheck(pc_before + 1, 82);   // checking that the PC was incremented by 1
         
         //STR r0, r0, #1
         clkR;
