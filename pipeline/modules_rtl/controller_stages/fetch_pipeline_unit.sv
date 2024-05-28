528683cc5a8a9983ab80e5ca4b43f59f5d23ef0a
Richard Sun <richardsun.gz@gmail.com>
2024-05-27 20:12:19 -0700

    fetch stages store the PC

--- pipeline/modules_rtl/controller_stages/fetch_pipeline_unit.sv
+++ pipeline/modules_rtl/controller_stages/fetch_pipeline_unit.sv
@@ -3,19 +3,33 @@ module fetch_pipeline_unit(
     input clk,
     input rst_n,
     input branch_in,
-    output branch_value
+    input [6:0] pc_in,
+    output branch_value,
+    output [6:0] pc_out
     // controller signals
     // NOTHING for now
 );
 // pipeline unit ports
 reg branch_value_reg;
+reg [6:0] pc_reg;
 assign branch_value = branch_value_reg;
+assign pc_out = pc_reg;
+
 
 always_ff @( posedge clk or negedge rst_n) begin
     if (~rst_n) begin
         branch_value_reg <= 1'b1; // pass the value of 1 to the first stage load_pc_start to squash the instruction
     end else begin
         branch_value_reg <= branch_in;
+    end
+end
+
+// pc register
+always_ff @( posedge clk or negedge rst_n) begin
+    if (~rst_n) begin
+        pc_reg <= 7'd0;
+    end else begin
+        pc_reg <= pc_in;
     end
 end
 
